1b138130c1fa2e2364e22c5fcaa98f1df4bc58b8
Akash Kumar <akashpk1997@gmail.com>
2020-03-24 21:57:37 +0530

    Added Google Authentication

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT;
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -15,6 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+
+
 app.use(session({
     secret: "Hey my name is Akash.",
     resave: false,
@@ -24,17 +25,21 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+if(process.env.ENV === "PROD")
+{
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbURL = 'mongodb+srv://' + dbUser + ':' + dbPass + '@cluster0-qnmpu.mongodb.net/secretsDB';
-
-
 mongoose.connect(dbURL,  { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false});
+} else {
+    mongoose.connect("mongodb://localhost:27017/secretsDB", { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false});
+}
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -43,21 +48,43 @@ userSchema.plugin(findOrCreate);
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
 
+if(process.env.ENV === "PROD") {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "https://evening-atoll-03139.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
-  function(accessToken, refreshToken, profile, cb) {
+  function(accessToken, refreshToken, profile, cb) {    
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
+} else {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        const proName = profile.name.givenName;
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+}
 
 app.get("/", (req, res) => {
     res.render('home');
@@ -65,6 +92,12 @@ app.get("/", (req, res) => {
 
 app.get("/auth/google", 
     passport.authenticate('google', {scope: ['profile']}));
+
+app.get("/auth/google/secrets", 
+    passport.authenticate('google', { failureRedirect: '/login'}),
+    (req, res) => {
+        res.redirect('/secrets');
+    })
 
 app.get("/login", (req, res) => {
     res.render('login');
@@ -117,8 +150,16 @@ app.post("/login", (req, res) => {
     });
 });
 
-
-app.listen(port, () =>{
-    console.log("App started at localhost:" + port);
-    
-})
+if(process.env.ENV === "PROD"){
+    const port = process.env.PORT;
+    app.listen(port, () =>{
+        console.log("App started at localhost:" + port);
+        
+    })
+} else {
+    const port = 3000 || process.env.PORT;
+    app.listen(port, () =>{
+        console.log("App started at localhost:" + port);
+        
+    })
+}
